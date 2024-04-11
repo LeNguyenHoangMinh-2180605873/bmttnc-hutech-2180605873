b9e86cf12177ad11b3a74b0e834b7c29ebaf8875
minhle1907 <118109154+minhle1907@users.noreply.github.com>
2024-04-11 22:55:00 +0700

    [add]

--- lab_02/ex01/api.py
+++ lab_02/ex01/api.py
@@ -2,6 +2,7 @@ from flask import Flask, request,jsonify
 from cipher.caesar import CaesarCipher
 from cipher.railfence import RailFenceCipher
 from cipher.Playfair import PlayFairCipher
+from cipher.transposition import TranspositionCipher
 app= Flask(__name__)
 
 
@@ -71,6 +72,26 @@ def playfair_decrypt():
     plainfare_matrix = playfair_cipher.create_playfair_matrix(key)
     decrypt_text= playfair_cipher.playfair_decrypt(cipher_text,plainfare_matrix)
     return jsonify({"decrypted_text ":decrypt_text})
+
+#Transposition cipher
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text=data.get("plain_text")
+    key=int(data.get('key'))
+    encrypted_text=transposition_cipher.encrypt(plain_text,key)
+    return jsonify({'encrypted_text':encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data=request.get_json()
+    cipher_text=data.get("cipher_text")
+    key= int(data.get('key'))
+    decrypted_text=transposition_cipher.decrypt(cipher_text,key)
+    return jsonify({'decrypted_text':decrypted_text})
+
 #main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000, debug=True)
